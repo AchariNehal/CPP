ed3db410e743381d3b2830ffe444b07be870ed6d
AchariNehal <nehalachari25@gmail.com>
2026-03-25 21:59:43 +0530

    inverted pyramid

--- pattern.cpp
+++ pattern.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++)
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i-1;j++)
             cout << " ";
-        for(int k=0;k<(2*i)+1;k++)
+        for(int k=0;k<2*(n-i)+1;k++)
             cout << "*";
     cout << "\n";
     }
